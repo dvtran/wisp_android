f933f5371ae10c7001e12ea3752df129046b53b7
thyrfa <l,cthyrfa@gmail.com>
2013-07-15 15:43:56 -0400

    Added a storage wrapper class

---
+++ src/com/example/wisputil/Storage.java
@@ -0,0 +1,18 @@
+package com.example.wisputil;
+
+import java.io.Serializable;
+
+import android.location.Location;
+
+public class Storage implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7703373910652239983L;
+	Location location;
+	Byte[] sound;
+	public Storage(Location loc, Byte[] s){
+		location=loc;
+		sound=s;
+	}
+}
