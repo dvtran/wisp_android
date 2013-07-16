e288b04080d66473c4745915eec64b41d62c1a60
thyrfa <l,cthyrfa@gmail.com>
2013-07-16 14:42:57 -0400

    Work on map showing, not done

--- src/com/example/wispdisplay/MapShow.java
+++ src/com/example/wispdisplay/MapShow.java
@@ -1,13 +1,40 @@
 package com.example.wispdisplay;
 
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.Window;
+
 import com.example.wisp.R;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 
-public class MapShow {
+public class MapShow extends Activity {
 	private GoogleMap map;
 	public MapShow(){
-		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 
+	}
+	public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_map);
+		MapFragment mMapFragment = MapFragment.newInstance();
+		FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+		fragmentTransaction.add(R.id.map, mMapFragment);
+		fragmentTransaction.commit();
+		setUpMap();
+		map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+	}
+	private void setUpMap() {
+	    // Do a null check to confirm that we have not already instantiated the map.
+	    if (map == null) {
+	        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+	                            .getMap();
+	        // Check if we were successful in obtaining the map.
+	        if (map != null) {
+	            // The Map is verified. It is now safe to manipulate the map.
+
+	        }
+	    }
 	}
 }
